8194d516cec14c90a7348e5896573fb002e9fda6
kreca <kreca>
2008-08-11 09:32:03 +0000

    Fix bug in /whoisform that accepted invalid selections.

--- format/whois/config.m
+++ format/whois/config.m
@@ -6,9 +6,8 @@ alias whoisform.show {
 };
 
 ## constructor
-alias format.loadwhois (number,void)
-{
-	if (fexist($(loadpath)format/whois/whois.$number)) {
+alias format.loadwhois (number,void) {
+	if (fexist($(loadpath)format/whois/whois.$number) != -1) {
 		load ${loadpath}format/whois/whois.$number;
 		@theme.format.whois = number;
 		return 1;
@@ -21,6 +20,8 @@ alias whoisform (number,void) {
 	if (@number) {
 		if (format.loaditem(whois $number)) {
 			xecho $acban whois format set to $theme.format.whois;
+		}{
+			xecho -b please make a valid selection.;
 		};
 	}{
 		whoisform.show;
