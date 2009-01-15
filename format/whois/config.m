eaf537b1b7dc656b5a80172e16e0df9e0196245e
kreca <kreca>
2009-01-15 18:11:14 +0000

    Remove the need of whois.ans

--- format/whois/config.m
+++ format/whois/config.m
@@ -1,10 +1,6 @@
 # Copyright (c) 2003-2009 Amnesiac Software Project.
 # See the 'COPYRIGHT' file for more information.
 
-alias whoisform.show {
-	load $(loadpath)format/whois/whois.ans;
-};
-
 ## constructor
 alias format.loadwhois (number,void) {
 	if (fexist($(loadpath)format/whois/whois.$number) != -1) {
@@ -14,6 +10,23 @@ alias format.loadwhois (number,void) {
 	}{
 		return 0;
 	};
+};
+alias whoisform.show (void) {
+	@:currentwhois=theme.format.whois;
+	for (@:mm=1, mm <= 10, @mm++) {
+		aecho %K\[%n$mm%K\]%n;
+		echo;
+		@format.loaditem(whois $mm);
+		//shook 311 . zak cum i.love.androgyns@san.francisco * crapple;
+		//shook 301 . Crying while eating
+		//shook 319 . . @#bearcave +#bignuts #freebsd
+		//shook 312 . . irc.cripplekids.org eurotrash's are cool
+		//shook 313 . zak;
+		//shook 317 . zak 666 0
+		//shook 318;
+		echo;
+	};
+	@format.loaditem(whois $currentwhois);
 };
 
 alias whoisform (number,void) {
