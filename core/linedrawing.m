d2ce7fd7c2061fa8391683d5d518673344c4cf02
kreca <kreca>
2009-06-23 17:52:43 +0000

    Oki, did a bugfix to the old code Can someone that _was_ affected by the bug please type /set_line_drawing 1 and check how the sbars look after that. please switch between /eval set_line_drawing 1;sbar 1; and /eval set_line_drawing 0;sbar 1; to see if there is any difference KTHX

--- core/linedrawing.m
+++ core/linedrawing.m
@@ -7,27 +7,38 @@ if (word(2 $loadinfo()) != [pf]) {
 };
 subpackage linedrawing;
 
-@ld.e = '';
-@ld.asc_chars = getcap(TERM acs_chars 0 0);
-
+alias set_line_drawing (mode, void) {
+	if (mode == 1 ) {
+		@ld.e = '';
+		@ld.asc_chars = getcap(TERM acs_chars 0 0);
+	} 
+	else {
 # Hard coded vars for ibm437. Above does not play well with status bars
-@ld.asc_chars ="j$chr(217)k$chr(191)l$chr(218)m$chr(192)q$chr(196)t$chr(195)u$chr(180)v$chr(193)w$chr(194)x$chr(179)~$chr(250)";
-@ld.e = '';
-fec ($ld.asc_chars) vt cme {
-	@:me="${ld.e}${cme}${ld.e}";
-	switch ($vt) {
-		(w) { @:cVar ='tee_down'; };
-		(u) { @:cVar ='tee_left'; };
-		(t) { @:cVar ='tee_right'; };
-		(v) { @:cVar ='tee_up'; };
-		(l) { @:cVar ='ul_corner'; };
-		(k) { @:cVar ='ur_corner'; };
-		(m) { @:cVar ='ll_corner'; };
-		(j) { @:cVar ='lr_corner'; };
-		(x) { @:cVar ='vline'; };
-		(q) { @:cVar ='hline'; };
-		(~) { @:cVar ='smalldot'; };
+		@ld.asc_chars ="j$chr(217)k$chr(191)l$chr(218)m$chr(192)q$chr(196)t$chr(195)u$chr(180)v$chr(193)w$chr(194)x$chr(179)~$chr(250)";
+		@ld.e = '';
 	};
-	@ld[$cVar]=me;
-	@ld[$(cVar)_e]=cme;
+	fec ($ld.asc_chars) vt cme {
+		@:me="${ld.e}${cme}${ld.e}";
+		@:cVar ="";
+		switch ($vt) {
+			(w) { @:cVar ='tee_down'; };
+			(u) { @:cVar ='tee_left'; };
+			(t) { @:cVar ='tee_right'; };
+			(v) { @:cVar ='tee_up'; };
+			(l) { @:cVar ='ul_corner'; };
+			(k) { @:cVar ='ur_corner'; };
+			(m) { @:cVar ='ll_corner'; };
+			(j) { @:cVar ='lr_corner'; };
+			(x) { @:cVar ='vline'; };
+			(q) { @:cVar ='hline'; };
+			(~) { @:cVar ='smalldot'; };
+		};
+		if (@cVar) {
+			@ld[$cVar]=me;
+			@ld[$(cVar)_e]=cme;
+		};
+	};
 };
+
+
+@set_line_drawing(0);
