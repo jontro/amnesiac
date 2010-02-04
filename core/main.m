bfa0d188ac1285e82b528649f49a19b32b3fb456
zak <zak>
2010-02-04 20:25:37 +0000

    remove adcc from pasture for good, no functional changes

--- core/main.m
+++ core/main.m
@@ -13,8 +13,8 @@ subpackage main;
 @a.rel_id='1';
 @a.rel="2.0.$a.rel_id";
 @a.date='20091104';
-@a.snap='20100203';
-@a.commitid='30';
+@a.snap='20100204';
+@a.commitid='31';
 
 ## color vars.
 @ cl    = "[0m";
