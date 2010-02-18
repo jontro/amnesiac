38ac23124ccdbf1415e66b1594e8f7f5998faa2b
zak <zak>
2010-02-18 08:34:20 +0000

    -somehow during the merge of eimodes/bans.m into chanmodes.m bans.m did not make it, also merge channel.m into chanmodes.m purging more duplicates *sigh*, now amnesiac has /bk /bantype ban support etc again sorry for the mess. -Also another note, between epic5 cvs 1620-21(server handling names got  broken, it's fixed in 1622, but other bugs have now popped up cvs users of  epic5 should stick with 1619, until the bugs are corrected. you can find a  copy at http://amnesiac.ircii.org/epic5/cvs if you do not know how to revert  with cvs.

--- core/chanmodes.m
+++ core/chanmodes.m
@@ -34,6 +34,24 @@ alias pinv ui;
 alias pexp ue;
 alias smhelp rathelp;
 
+## bans. (functionality neear the end of the line!)
+alias bki {bk $*;ignore $0 msgs;};
+alias kbi {kb $*;ignore $0 msgs;};
+alias bkt {qk $*;};
+alias bk {bankick $*;};
+alias kb {bankick $*;};
+alias unban {ub $*;}
+alias clearbans {ub $*;};
+
+alias banstat bans;
+alias sb bans;
+
+## ban aliases.
+alias fuckem lban;
+alias lban {
+	//mode $serverchan() +bbb *!*@0.0.0.0/1 *!*@128.0.0.0/1 *!*@0000::/1 $0 $0 $0 $0;
+};
+
 ## chan info.
 alias scano {sco $*;};
 alias scanv {scv $*;};
@@ -505,6 +523,140 @@ alias ieh (nick){@_iexp(ieh 2 $nick);};
 alias ieb (nick){@_iexp(ieb 3 $nick);};
 alias ied (nick){@_iexp(ied 4 $nick);};
 alias iiexp (nick){@_iexp(iiexp 10 $nick);};
+
+## ban functionality
+
+## generic mode +b alias. Used all over the place
+alias _uhbmode (bt,chan, ...) {
+	if ("$3@$4"!='<UNKNOWN>@<UNKNOWN>') {
+		//mode $chan +b $mask($bt $0!$3@$4);
+	};
+};
+alias _bmode (bt,nick ,void) {
+	fe ($split(, $nick)) cn {
+		if (userhost($cn) == '<UNKNOWN>@<UNKNOWN>') {
+			userhost $cn -cmd  \{ @ _uhbmode\($bt $serverchan() $$*\)\};
+		}{
+			//mode $serverchan() +b $mask($bt $cn!$userhost($cn));
+		};
+	};
+};
+
+## Different ban aliases starting here
+alias ban (nick,void){
+	if (!@nick) {
+		xecho -b Usage: /ban nick;
+	}{
+		fe ($split(, $nick)) cn {
+			if (match(*!*@* $cn)) {
+				//mode $serverchan() +b $cn;
+			}{
+				@_bmode($_bt $cn);
+			};
+		};
+	};
+};
+
+# Generic bk alias. Used below
+alias _bk (aliasname,bt,nick, reason){
+	if (!@bt || !@nick) {
+		xecho -b Usage: /$aliasname nick1,nick2 [reason];
+		return;
+	};
+	@_bmode($bt $nick);
+	if (@reason) {
+		kick $nick $reason;
+	}{
+		kick $nick;
+	};
+};
+
+alias bankick (nick, reason) {
+	@_bk(bankick $_bt $nick $reason);
+};
+
+## basically this allows you to specify the bantype you want to
+## use on the fly without needing to set /bantype instead now we have
+## /bkh(host) /bkb(better) /bkn(normal) /bkd(domain) followed by nick.
+## Numbers used below is $mask(...) types
+alias bkh (nick, reason){@_bk(bkh 2 $nick $reason);};
+alias bkb (nick, reason){@_bk(bkb 3 $nick $reason);};
+alias bkn (nick, reason){@_bk(bkn 6 $nick $reason);};
+alias bkd (nick, reason){@_bk(bkd 4 $nick $reason);};
+## iban alias. (i stands for ident)
+alias iban (nick, reason){@_bk(iban 10 $nick $reason);};
+
+## end bankick type on the fly.
+alias kickban (nick,reason){
+	if (!@nick) {
+		xecho -b Usage: /kickban nick1,nick2 [reason];
+        }{
+		if ( @reason) {
+			kick $serverchan() $nick $reason;
+		}{
+			kick $serverchan() $nick;
+                };
+		@_bmode($_bt $nick);
+	};
+
+};
+
+# temp bk
+alias qk (nick,reason) {
+	if (!@nick) {
+		xecho $acban Usage: /qk <nick> <reason> - will bankick specified nick then unban them after 5 seconds.;
+	}{
+		if (@reason) {
+			@bankick($nick $reason);
+		}{
+			@bankick($nick);
+		};
+		^timer 5 \{ub $nick\};
+	};
+};
+
+## end kicks.
+
+## config
+
+alias kops {
+	config.kickops -s $*;
+};
+
+alias config.kickops {
+	if ( *0 == '-r' ) {
+		return $kickops;
+	} else if (*0 == '-s') {
+		config.matchinput on|off kickops '$1' kick ops on ban;
+	};
+};
+
+alias bantype {
+	config.bantype -s $*;
+};
+
+## bantype config. [Norm/Better/Host/Domain]
+alias config.bantype {
+	if ( *0 == '-r' ) {
+		return $bt_;
+	} else if (*0 == '-s') {
+		if (# == 1) {
+			xecho -v $acban bantype set to $bt_;
+			xecho -v $acban /bantype <Normal|Better|Host|Domain>;
+		}{
+			switch ($1) {
+			(normal){@bt_='normal';@_bt=6;xecho -v $acban bantype set to normal (n!*u@h.d n!*u@d.h);}
+			(better) {@bt_='better';@_bt=3;xecho -v $acban bantype set to better (*!*u@*.d *!*u@d.*);}
+			(host) {@bt_='host';@_bt=2;xecho -v $acban bantype set to host (*!*@h.d *!*@d.h);}
+			(domain) {@bt_='domain';@_bt=4;xecho -v $acban bantype set to domain (*!*@*.d *!*@d.*);}
+			() {xecho -v $acban invalid choice <Normal|Better|Host|Domain>;}
+			};
+		};
+	};
+};
+
+osetitem protect kickops Kick ops:;
+osetitem protect bantype Ban Type:;
 
 alias rathelp {
 //echo -----------------= Special Modes Help =----------------------------;
--- core/main.m
+++ core/main.m
@@ -13,8 +13,8 @@ subpackage main;
 @a.rel_id='1';
 @a.rel="2.0.$a.rel_id";
 @a.date='20091104';
-@a.snap='20100204';
-@a.commitid='32';
+@a.snap='20100218';
+@a.commitid='33';
 
 ## color vars.
 @ cl    = "[0m";
