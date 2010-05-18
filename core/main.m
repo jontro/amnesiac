e94da51da5dd970404cf642b92fac94bd8d7a0b4
zak <zak>
2010-05-18 04:16:52 +0000

    -add statusbar 8 modification to add activity support, if some dont like it ill make another statbar like it with activity support if needed, which hopefully wont be the case, patch submitted by ryan, comitted by me //zak

--- core/main.m
+++ core/main.m
@@ -13,8 +13,8 @@ subpackage main;
 @a.rel_id='1';
 @a.rel="2.0.$a.rel_id";
 @a.date='20091104';
-@a.snap='20100516';
-@a.commitid='48';
+@a.snap='20100518';
+@a.commitid='49';
 
 ## color vars.
 @ cl    = "[0m";
